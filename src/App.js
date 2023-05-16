ffb3fcb7d0b567e7f0295716ada8605180c73791
SanduniDR <sandunidilshika@gmail.com>
2023-05-16 22:49:54 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import PartOne from './components/PartOne'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> First REACT Project </h1>
+      <PartOne />
+      <PartOne/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PartOne.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PartOne() {
+    return (
+        <div>
+            <h1>Welcome Home! new Componenet!</h1>
+        </div>
+    );
+}
